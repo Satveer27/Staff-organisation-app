c4dc9b0511a423e404c1e086640d9c7388ca8b2c
2785448g <2785448g@student.gla.ac.uk>
2024-01-05 20:15:40 +0800

    Completed setting up folder structure
    Completed setting up folder structure for backend of the web application

---
+++ Organisation-backend/server.js
@@ -0,0 +1,9 @@
+//run the application, all other logic go app
+//This app uses MVC structure
+import http from 'http';
+import app from './app/app.js'
+
+//create server
+const PORT = process.env.PORT || 7000
+const server = http.createServer(app);
+server.listen(PORT, console.log(`Server is running on port ${PORT}`));
